525efb7bbfed67e9297e17f2cec9fbfef9cc02c6
koss <fikuskos@gmail.com>
2019-09-30 01:42:50 +0300

    added (admin) strucure html and css

--- resources/ts/components/example.tsx
+++ resources/ts/components/example.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 interface Props {
     count?: number;
+    text?: string;
 }
 
 interface State {
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,60 @@
 @extends('layouts.admin')
 
 @section('content')
-<div id="admin-app">
+<div id="admin-app" class="main-wrapper">
+    <div class="side-bar">
+
+    </div>
+    <div class="right-column">
+        <nav class="header">
+            <div class="search">
+                <div class="search__logo">
+                    <input
+                            class="input-search"
+                            type="search"
+                            name="search"
+                            placeholder="Search"
+                    >
+                    <i class="fas fa-search"></i>
+                </div>
+            </div>
+            <div class="header__item">
+                <i class="fab fa-angellist"></i>
+                <div class="item__sub-menu">
+                    <a href="" class="sub-menu__item">chto-to tam to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                </div>
+            </div>
+            <div class="header__item">
+                <i class="fas fa-cog"></i>
+                <div class="item__sub-menu">
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                </div>
+            </div>
+            <div class="header__item">
+                <i class="fas fa-bell"></i>
+                <div class="item__sub-menu">
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                </div>
+            </div>
+            <div class="header__item">
+                <i class="fas fa-user"></i>
+                <div class="item__sub-menu">
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                    <a href="" class="sub-menu__item">chto-to tam</a>
+                </div>
+            </div>
+        </nav>
+    </div>
 </div>
 @endsection
